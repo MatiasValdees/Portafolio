3efa47bb15ae8345574009c5a5bbac9a95d573cd
MatiasValdees <matias.valdesr3@gmail.com>
2024-03-08 11:42:14 -0300

    navbar ok!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Banner } from './components/Banner/Banner'
+import { Navbar } from './components/Navbar/Navbar'
 import { Card } from './components/Card/Card'
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Banner/>
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './Narbar.css'
+export const Navbar = () => {
+
+    const [toggle,setToggle]=useState(false);
+
+    const onToggle=()=>{
+        setToggle(!toggle)
+    }
+
+    return (<>
+        <div className="open-navbar">
+            <i className="fa-solid fa-bars border-line" onClick={()=>onToggle()}></i>
+        </div>
+        <nav className="navbar" style={{left:toggle?'-100%':'0%'}}>
+            <div className="items-navbar">
+                <div className='mark-x'><i className="fa-solid fa-x" onClick={()=>onToggle()}></i></div>
+                <div className="icon-banner">
+                    <h1>Portalio</h1>
+                </div>
+                <ul className='list-navbar'>
+                    <li className='item-navbar'>Home</li>
+                    <li className='item-navbar'>trajectory</li>
+                    <li className='item-navbar'>Projects</li>
+                    <li className='item-navbar'>Lenguages</li>
+                    <li className='item-navbar'>About me</li>
+                    <li className='item-navbar'>Contact</li>
+                </ul>
+                <div className="social">
+                    <i className="fa-brands fa-github icon-social"></i>
+                    <i className="fa-brands fa-linkedin icon-social"></i>
+                </div>
+            </div>
+        </nav>
+    </>);
+}
